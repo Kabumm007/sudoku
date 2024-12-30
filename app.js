840f3bffb9422b654cd3edfeda92b4fb04e59865
Kabumm007 <thomas.galweiler@gmail.com>
2024-12-30 13:18:12 +0100

    max zahl aussuchen

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ function Sleep(milliseconds) {
     let b=0
     let c=0
     let korrektfeld=document.getElementById("korrekt")
-    //Zufallszahl von 0 bis 9
+    let maxzahl= prompt("gebe den maxinalen ziffernbereich ein") 
     while(true){
-        a=Math.floor(Math.random() * 10);
-        b=Math.floor(Math.random() * 10);
+        a=Math.floor(Math.random() * maxzahl);
+        b=Math.floor(Math.random() * maxzahl);
         c=a*b
         let antwort=prompt("was ist "+a+" mal "+b)
         if(c==antwort) korrektfeld.innerHTML="richtig"
