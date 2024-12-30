dada2fcf1519149032a14af906a9b2c3718669f1
Kabumm007 <thomas.galweiler@gmail.com>
2024-12-30 10:51:53 +0100

    service worker aus

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 
-
-alert("hallo")
+let a=0
+let b=0
+let c=0
+let korrektfeld=document.getElementById("korrekt")
+//Zufallszahl von 0 bis 9
+a=Math.floor(Math.random() * 10);
+b=Math.floor(Math.random() * 10);
+c=a*b
+let antwort=prompt("was ist "+a+" mal "+b)
+korrektfeld.innerHTML="richtig"
